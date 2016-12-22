d809e2004d92628621b982cdd67bf3bdf9f39557
Elizabeth Sanger <libsang@gmail.com>
2016-12-22 12:18:28 -0600

    added third gallery, added contact icons

--- js/scripts.js
+++ js/scripts.js
@@ -74,5 +74,20 @@
             tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
         }
     });
+
+     $('.popup-gallery3').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        }
+    });
 })(jQuery);
 
